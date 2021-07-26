5f077f4a7bcb111c48f77f1a6caf2921167f0583
michaelmccoll <mccollmichael@hotmail.com>
2021-07-26 17:12:27 +0100

    More React features

--- App.js
+++ App.js
@@ -1,91 +1,205 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Platform, StyleSheet, TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Sharing from 'expo-sharing';
 import uploadToAnonymousFilesAsync from 'anonymous-files';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
 import { render } from 'react-dom';
+
+import * as React from 'react';
+import { Button, View, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 // To  add delay to splash screen
 // import * as SplashScreen from 'expo-splash-screen';
-
 // SplashScreen.preventAutoHideAsync();
 // setTimeout(SplashScreen.hideAsync, 3000);
 
-export default function App() {
- 
-  const [selectedImage, setSelectedImage] = React.useState(null);
 
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 40, height: 40 }}
+      source={require('/Users/michaelmccoll/projects/expo/my-app/assets/favicon.png')}
+    />
+  );
+}
 
-    if (permissionResult.granted === false) {
-      alert("Permission to access camera roll is required!");
-      return;
-    }
+function HomeScreen({navigation,route}) {
 
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-    if (Platform.OS === 'web') {
-      let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri });
-    } else {
-      setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
-    }
-  };
+  React.useEffect(() => {if (route.params?.post){}}, [route.params?.post]);
 
-  // Button to remove selected image
-  let removeSelectedImage = async () => {
-    setSelectedImage({ selectedImage: null })
-  }
+  // CREATES a counter on header
+  // const [count, setCount] = React.useState(0);
+  // React.useLayoutEffect(() => {
+  //   navigation.setOptions({
+  //     headerRight: () => (
+  //       <Button onPress={() => setCount(c => c + 1)} title="Update count" />
+  //     ),
+  //   });
+  // }, [navigation]);
 
-  let openShareDialogAsync = async () => {
-    if (!(await Sharing.isAvailableAsync())) {
-      alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
-      return;
-    }
-
-    await Sharing.shareAsync(selectedImage.localUri);
-  };
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{ uri: selectedImage.localUri }} style={styles.thumbnail}/>
-        <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Share this photo</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={removeSelectedImage} style={styles.button}>
-          <Text style={styles.buttonText}>Remove this photo</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Select Another Picture</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+  // return <Text>Count: {count}</Text>;
 
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}/>
-        <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
-        <TouchableOpacity
-          onPress={openImagePickerAsync} style={styles.button}>
-          <Text style={styles.buttonText}>Pick a photo</Text>
-        </TouchableOpacity>
-      </NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Create post" onPress={() => navigation.navigate('CreatePost')}/>
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+      
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details',
+          {itemId: 86,
+          otherParam: 'anything you want here',})}/>
     </View>
   );
-
-
-
 }
+
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }} value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true,});
+          }}
+      />
+    </>
+  );
+}
+
+
+function DetailsScreen({route, navigation}) {
+  const {itemId, otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button title="Go to Details... again" onPress={() => navigation.push('Details',{
+            itemId: Math.floor(Math.random() * 100)})}/>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')}/>
+      <Button title="Go back to first screen in stack" onPress={() => navigation.popToTop()}/>
+      <Button title="Go back" onPress={() => navigation.goBack()}/>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>      
+      <Stack.Navigator screenOptions={{
+        headerStyle: {backgroundColor: '#f41e29',},
+        headerTintColor: '#fff',
+        headerTitleStyle: {fontWeight: 'bold',},
+        }}
+      >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerTitle: props => <LogoTitle {...props} />,
+          headerRight: () => (
+            <Button onPress={() => alert('This is a button!')} title="Info" color="#fff"/>
+          ),
+        }}
+        />
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} options={({ route }) => ({ title: route.params.name })}/> */}
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
+// *** IMAGE UPLOAD APP ***
+// export default function App() {
+ 
+//   const [selectedImage, setSelectedImage] = React.useState(null);
+
+//   let openImagePickerAsync = async () => {
+//     let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+//     if (permissionResult.granted === false) {
+//       alert("Permission to access camera roll is required!");
+//       return;
+//     }
+
+//     let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    
+//     if (pickerResult.cancelled === true) {
+//       return;
+//     }
+//     if (Platform.OS === 'web') {
+//       let remoteUri = await uploadToAnonymousFilesAsync(pickerResult.uri);
+//       setSelectedImage({ localUri: pickerResult.uri, remoteUri });
+//     } else {
+//       setSelectedImage({ localUri: pickerResult.uri, remoteUri: null });
+//     }
+//   };
+
+//   // Button to remove selected image
+//   let removeSelectedImage = async () => {
+//     setSelectedImage({ selectedImage: null })
+//   }
+
+//   let openShareDialogAsync = async () => {
+//     if (!(await Sharing.isAvailableAsync())) {
+//       alert(`The image is available for sharing at: ${selectedImage.remoteUri}`);
+//       return;
+//     }
+
+//     await Sharing.shareAsync(selectedImage.localUri);
+//   };
+
+//   if (selectedImage !== null) {
+//     return (
+//       <View style={styles.container}>
+//         <Image
+//           source={{ uri: selectedImage.localUri }} style={styles.thumbnail}/>
+//         <TouchableOpacity onPress={openShareDialogAsync} style={styles.button}>
+//           <Text style={styles.buttonText}>Share this photo</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity onPress={removeSelectedImage} style={styles.button}>
+//           <Text style={styles.buttonText}>Remove this photo</Text>
+//         </TouchableOpacity>
+//         <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+//           <Text style={styles.buttonText}>Select Another Picture</Text>
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <NavigationContainer>
+//         <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo}/>
+//         <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+//         <TouchableOpacity
+//           onPress={openImagePickerAsync} style={styles.button}>
+//           <Text style={styles.buttonText}>Pick a photo</Text>
+//         </TouchableOpacity>
+//       </NavigationContainer>
+//     </View>
+//   );
+// }
+// *** IMAGE UPLOAD APP ***
+
 
 const styles = StyleSheet.create({
   container: {
